dfe6903a4f3557fd027f7fd376b1c392af7aef9b
NeylaP <neyla1998@outlook.es>
2024-05-15 20:49:08 -0500

    .

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 4000;
+console.log("aca");
+app.get('/api/healthy', (req, res)=>{
+    console.log("Hola");
+    res.status(200).json({
+        success: true,
+        message: "My server is healthy"
+    });
+})
 
 app.listen(PORT, () => {{
     console.log(`Serve listening on port: ${PORT}`);
